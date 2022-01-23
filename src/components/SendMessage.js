fb530334dc443bf2a8656e01c2c1c07cb77c5e35
Cosmic-rare <ysolakey1116@gmail.com>
2022-01-23 14:35:51 +0900

    🎸 ChangeInput

--- src/components/SendMessage.js
+++ src/components/SendMessage.js
@@ -1,19 +1,17 @@
 import TextField from "@material-ui/core/TextField"
 import Button from "@material-ui/core/Button"
+import TextareaAutosize from '@material-ui/core/TextareaAutosize'
 
 const SendMessage = (props) => {
   const { activeTopic } = props
   return (
     <div className="send-messages-grid">
       <div className="send-message-container">
-        <TextField
-          autoComplete="off"
-          color="primary"
-          id="filled-name"
-          className="message-input"
-          label={`Message # ${activeTopic}`}
+        <TextareaAutosize
+          aria-label="empty textarea"
+          placeholder={`Message  #${activeTopic}`}
+          className="message-text-area"
         />
-        <Button color="primary" variant="contained" className="message-button">Send</Button>
       </div>
     </div >
   )
